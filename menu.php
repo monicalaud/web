4a1009935dbb8cf7aa96e86f7b837e51b3b2cc39
monica.laud <monica.laud@khk.ee>
2017-03-22 15:30:33 +0200

    menüü koostamine

---
+++ menu.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: monica.laud
+ * Date: 22.03.2017
+ * Time: 15:26
+ */
+//loome menüü templaitid
+$menu = new template('menu.menu');
+$item = new template('menu.item');
+//kontrollime objekti sisu
+echo '<pre>';
+print_r($menu);
+print_r($item);
+echo '<pre>';
+?>
