1227d2975c25b1b911487c1985ee5e0be953e990
monica.laud <monica.laud@khk.ee>
2017-03-22 12:29:14 +0200

    lisatud faiulist lugemise funktsioon

--- classes/template.php
+++ classes/template.php
@@ -12,6 +12,12 @@ class template
     var $file = ''; //template fail(main)
     var $content = false; //sisu veel puudub, html faili sisu
     var $vars = array(); //tyhi massiiv, html vaate sisu, reaalsed väärtused
+
+    // loeme faili sisu html faili  mallist
+    function readFile($f)
+    {
+        $this->content = file_get_contents($f); //html faili sisu , contendid tulid juba valikust
+    }//readFile
 }
 
 classi lõpp
