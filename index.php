f4b536b2bd5112e3d12cbaba89c22cd8233756e6
monica.laud <monica.laud@khk.ee>
2017-03-22 14:57:41 +0200

    väljastus

--- index.php
+++ index.php
@@ -17,6 +17,7 @@ $main_tmpl->set('title', 'pealkiri');
 $main_tmpl->set('lang-bar', 'keeleriba');
 $main_tmpl->set('menu', 'lehe peamenüü');
 $main_tmpl->set('content', 'sisu');
+$main_tmpl->set('site_title', 'Veebiprogrameerimine');
 //kontrollime antud objekti sisu
 
 
