ecf8e97b83fb8e968492a562fa5f68ab84f4ae16
monica.laud <monica.laud@khk.ee>
2017-03-22 12:45:27 +0200

    lisasime faili olem asolu kontrolli

--- classes/template.php
+++ classes/template.php
@@ -13,11 +13,26 @@ class template
     var $content = false; //sisu veel puudub, html faili sisu
     var $vars = array(); //tyhi massiiv, html vaate sisu, reaalsed väärtused
 
+    //html faili sisu lugemine
+    function loadFile()
+    {
+        $f = $this->file; //lokaalme asendus
+        //kontrollime kas template dir on olemas
+        if (!is_dir(TMPL_DIR)) {
+            echo 'Kataloogi.'TMPL_DIR'.ei leitud</br>';
+             exit;
+        }
+        //kui fail on olemas ja lugemiseks sobib , loeme failist malli sisu
+        $this->readFile($f);
+    }
+if ($this->content === false)
+{
+echo 'ei suutnud lugeda faili '.$this->file.'<br />';
+}// loadFile
     // loeme faili sisu html faili  mallist
     function readFile($f)
     {
         $this->content = file_get_contents($f); //html faili sisu , contendid tulid juba valikust
     }//readFile
 }
-
-classi lõpp
+//classi lõpp
