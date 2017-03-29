695ab5348e938809627f4886f2b9e4be8af55008
monica.laud <monica.laud@khk.ee>
2017-03-29 13:59:46 +0300

    http objekti loomine ja testimine

--- conf.php
+++ conf.php
@@ -7,9 +7,17 @@
  */
 // defineerime muutumatud asjad - konstandid
 // täiendame jooksvalt
-define('CALSSES_DIR', 'classes/'); //classes kataloogi konstant
+define('CLASSES_DIR', 'classes/'); //classes kataloogi konstant
 define('TMPL_DIR', 'tmpl/'); //template konstant. konstandid kirjutatakse alati suurte tähtedega
 //võtame kasutusele vajalikud failid
-require_once CALSSES_DIR . 'template.php';
+require_once CLASSES_DIR . 'template.php';
+require_once CLASSES_DIR . 'http.php';
+//LOOME     vajalikud objektid projekti tööks
+$http = new http();
+$http->init();
+//testime http objekti tööd
+echo '<pre>';
+print_r($http);
+echo '</pre>';
 ?>
 
