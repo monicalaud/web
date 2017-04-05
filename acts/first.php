9ad004883457c17533c4b12d395cba0ee973bb64
monica.laud <monica.laud@khk.ee>
2017-04-05 08:48:17 +0300

    loodud sisulehed ja kataloog

---
+++ acts/first.php
@@ -0,0 +1,11 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: monica.laud
+ * Date: 5.04.2017
+ * Time: 8:44
+ */
+$main_tmpl->set('content', 'Esimene leht');
+
+?>
---
+++ acts/second.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: monica.laud
+ * Date: 5.04.2017
+ * Time: 8:47
+ */
+$main_tmpl->set('content', 'Teine leht');
+?>
