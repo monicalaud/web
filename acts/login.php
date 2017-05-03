53aff7903eec6d9943c6e88a154609d66cceb6d3
monica.laud <monica.laud@khk.ee>
2017-05-03 09:25:09 +0300

    logimise loomine

--- acts/login.php
+++ acts/login.php
@@ -5,3 +5,15 @@
  * Date: 3.05.2017
  * Time: 9:08
  */
+//loome sisselogimisvormi objekti
+$login = new template('login');
+//paneme reaalsed väärtused
+$login->set('kasutajanimi', 'Kasutaja');
+$login->set('parool', 'Parool');
+$login->set('nupp', 'Logi sisse');
+
+//loome lingi
+$link = $http->getLink(array('act' => 'login_do'));
+$login->set('link', $link);
+// paneme sinu template sisse
+$main_tmpl->set('content', $login->parse());
